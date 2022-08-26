de5d4a1043edd41d49206a5af5babb9feabaf896
anmaogo <anmaogo@gmail.com>
2022-08-26 13:04:21 -0500

    ble

--- ESP32 Dev kit v1/BLE/BLE_server/BLE_server.ino
+++ ESP32 Dev kit v1/BLE/BLE_server/BLE_server.ino
@@ -3,6 +3,8 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
+BLEAdvertisementData advert;
+BLEAdvertising *pAdvertising;
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -12,6 +14,7 @@ void setup() {
   Serial.println("Starting BLE work!");
 
   BLEDevice::init("monitoreoBovino_BLE");
+  advert.setName("ESP32-new");
   BLEServer *pServer = BLEDevice::createServer();
   BLEService *pService = pServer->createService(SERVICE_UUID);
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
